a8ae7f49a906ca5abc5c035b0c1dacc9e70218cc
MadsOellgaard <madsoellgaard@hotmail.com>
2020-09-15 10:23:13 +0200

    Back to top Button

--- js/js.js
+++ js/js.js
@@ -6,10 +6,9 @@ console.log('JavaScript from js/js.js: up and running!');
 
 // Tjek at DOM er indlæst
 
-    // ... your code here ...
-    
-    // ... end ...
-});
+
+
+/*
 
 
 let myVideo = document.getElementById("myVideo");
@@ -19,36 +18,32 @@ myVideo.controls = true; //booleans J/N sandt/falsk
 
 // the ON / OFF trick
 function playPause() {
-  if (myVideo.paused) {
-    myVideo.play();
-    myVideo.controls = false;
-      
-    
-      }
+    if (myVideo.paused) {
+        myVideo.play();
+        myVideo.controls = false;
 
-     
-                          
-  else {
-    myVideo.pause();
-    myVideo.controls = true;
+
+    } else {
+        myVideo.pause();
+        myVideo.controls = true;
     }
-    
+
 
 
 }
 
 function mouseOver() {
-  document.getElementById("demo").style.color = "red";
+    document.getElementById("demo").style.color = "red";
 }
 
 function mouseOut() {
-  document.getElementById("demo").style.color = "black";
+    document.getElementById("demo").style.color = "black";
 }
 
 
 function makeBig() {
     myVideo.width = 1000;
-    
+
 }
 
 // muting sound
@@ -75,7 +70,36 @@ function makeNormal() {
 
 // eventlisteners will give you fewer codelines
 shutup.addEventListener('click',
-  function(){
-    myVideo.muted = true;
-  });
+    function () {
+        myVideo.muted = true;
+    });
 
+*/
+
+
+
+/* Scroll button functions */
+
+
+// Gets the button 
+var scrollbutton = document.getElementById("scrollbutton");
+
+
+// When you scroll past 2100px from the top of the document, the button will show itself, if below 2100px, the button is hidden
+function scrollFunction() {
+    if (document.body.scrollTop > 2100 || document.documentElement.scrollTop > 2100) {
+        scrollbutton.style.display = "block";
+    } else {
+        scrollbutton.style.display = "none";
+    }
+}
+
+window.onscroll = function() {
+    scrollFunction();
+}
+
+// When the user clicks on the button, scroll to 1150px from the top of the document At the map.
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 1150;
+}
