35d5ee246a2d99801bfda9ba194a6102f2d94cf1
Max Gaasvig <maxgaasvig@hotmail.com>
2020-09-16 11:32:53 +0200

    slideshow

--- js/js.js
+++ js/js.js
@@ -68,4 +68,36 @@ function topFunction() {
 
 
 
+var i = 0; 			// Start Point
+var images = [];	// Images Array
+var time = 5000;	// Time Between Switch
+	 
+// Image List
+images[0] = "images/slides1.jpg";
+images[1] = "images/slides2.jpg";
+images[2] = "images/slides3.jpg";
+images[3] = "images/slides4.jpg";
+
+
+
+// Change Image
+function changeImg(){
+	document.slide.src = images[i];
+
+	// Check If Index Is Under Max
+	if(i < images.length - 1){
+	  // Add 1 to Index
+	  i++; 
+	} else { 
+		// Reset Back To O
+		i = 0;
+	}
+
+	// Run function every x seconds
+	setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload=changeImg;
+
 
